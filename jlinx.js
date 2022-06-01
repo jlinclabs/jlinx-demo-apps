af1f66bfb4436d48aa28b38c5cc24cc78510e2e5
Jared Grippe <jared@deadlyicon.com>
2022-06-01 10:35:52 -0700

    setup jlinx client

--- jlinx.js
+++ jlinx.js
@@ -1,8 +1,14 @@
+const Path = require('path')
+const b4a = require('b4a')
+const appConfig = require('./appConfig')
 const JlinxClient = require('jlinx-client')
+
+console.log({appConfig})
 
 const jlinx = new JlinxClient({
   vaultPath: Path.join(appConfig.APP_DIR, 'jlinx.vault'),
-  vaultKey: appConfig.VAULT_KEY,
+  vaultKey: b4a.from(appConfig.jlinxVaultKey, 'hex'),
+  hostUrl: appConfig.jlinxHost
 })
 
 module.exports = jlinx
