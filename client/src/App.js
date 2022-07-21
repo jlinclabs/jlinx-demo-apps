f53456aba66eea0759e41597b66d05dc7781329f
Jared Grippe <jared@deadlyicon.com>
2022-07-21 13:12:48 -0700

    added MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import Button from '@mui/material/Button'
+import theme from './css/theme'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider {...{theme}}>
+      <CssBaseline />
+      <div className="App">
+        <Button variant="contained">nice</Button>
+      </div>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
